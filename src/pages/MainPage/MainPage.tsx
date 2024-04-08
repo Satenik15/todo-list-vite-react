4c4553a9b4f57f6d2193f365d96a52f335a611e5
Satenik15 <hakhverdyansatenik@gmail.com>
2024-04-08 23:35:30 +0400

    add file system

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+
+
+const MainPage = () => {
+   
+    return (
+        <div className="p-5">
+            <p className="text-2xl font-semibold">Main Page</p>
+            <p className="pt-3 text-slate-500">This is main page news</p>
+        </div>
+    )
+}
+
+export default MainPage;
